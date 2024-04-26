d894a0c011c74637ccb9a46c86bd99109f7d7583
MohaSetif <BouroubaMohamed004@gmail.com>
2024-04-26 18:18:02 +0100

    exporting prisma parser function

---
+++ utils/prismaParser.js
@@ -0,0 +1,42 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parsePrismaSchema = void 0;
+var fs = require("fs");
+function parsePrismaSchema(filePath) {
+    var schema = fs.readFileSync(filePath, 'utf-8');
+    var models = [];
+    var currentModel = null;
+    var lines = schema.split('\n');
+    var isInside = false;
+    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+        var line = lines_1[_i];
+        var trimmed_line = line.trim();
+        if (trimmed_line.startsWith('model ')) {
+            if (currentModel !== null) {
+                models.push(currentModel);
+            }
+            var modelName = trimmed_line.split(' ')[1];
+            currentModel = {
+                name: modelName,
+                attributes: []
+            };
+            isInside = true;
+        }
+        else if (trimmed_line == '}' && isInside) {
+            isInside = false;
+        }
+        else if (!trimmed_line.startsWith('//') && currentModel && isInside && trimmed_line) {
+            var attribute_line = trimmed_line.replace(/[{?}]/g, '');
+            var _a = attribute_line.split(/\s+/), attribute_name = _a[0], attribute_type = _a[1];
+            currentModel.attributes.push({
+                name: attribute_name,
+                type: attribute_type
+            });
+        }
+    }
+    if (currentModel != null) {
+        models.push(currentModel);
+    }
+    return models;
+}
+exports.parsePrismaSchema = parsePrismaSchema;
---
+++ utils/prismaParser.ts
@@ -0,0 +1,51 @@
+import * as fs from 'fs';
+
+interface Attribute {
+    name: string;
+    type: string;
+}
+
+interface Model {
+    name: string;
+    attributes: Attribute[];
+}
+
+export function parsePrismaSchema(filePath: string): Model[] {
+    const schema = fs.readFileSync(filePath, 'utf-8')
+    const models: Model[] = []
+    let currentModel: Model | null = null;
+    const lines = schema.split('\n')
+    let isInside : Boolean = false;
+
+    for(let line of lines){
+        const trimmed_line = line.trim();
+        if(trimmed_line.startsWith('model ')){
+            if (currentModel !== null) {
+                models.push(currentModel);
+            }
+            const modelName = trimmed_line.split(' ')[1];
+            currentModel = {
+                name: modelName,
+                attributes: []
+            }
+            isInside = true;
+        }
+        else if(trimmed_line == '}' && isInside){
+            isInside = false
+        }
+        else if(!trimmed_line.startsWith('//') && currentModel && isInside && trimmed_line){
+            const attribute_line = trimmed_line.replace(/[{?}]/g, '')
+            const [attribute_name, attribute_type] = attribute_line.split(/\s+/);
+            currentModel.attributes.push({
+                name: attribute_name,
+                type: attribute_type
+            })
+        }
+    }
+
+    if(currentModel != null){
+        models.push(currentModel)
+    }
+
+    return models
+}
