b810203de219ef8417e6065b1f03950cd09d168c
MohaSetif <BouroubaMohamed004@gmail.com>
2024-04-30 23:06:08 +0100

    Building the node CMD

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var createSvelteForm_1 = require("./utils/Svelte/createSvelteForm");
-var prismaParser_1 = require("./utils/prismaParser");
-var schemaFilePath = './prisma/User.prisma';
-var models = (0, prismaParser_1.parsePrismaSchema)(schemaFilePath);
-// models.forEach(model => {
-//     console.log(`Model: ${model.name}`);
-//     model.attributes.forEach(attribute => {
-//         console.log(`- Attribute: ${attribute.name}, Type: ${attribute.type}`);
-//     });
-//     console.log('\n');
-// });
-(0, createSvelteForm_1.createSvelteForm)(models);
+#!usr/bin/env node
+
+import inquirer from 'inquirer';
+
+async function getSchema() {
+    const questions = [
+        {
+            type: 'input',
+            name: 'schema_path',
+            message: 'Enter the path for the schema:',
+            default: './prisma/User.prisma'
+        },
+        {
+            type: 'input',
+            name: 'form_dest',
+            message: 'Enter the destination for the form:'
+        },
+        {
+            type: 'input',
+            name: 'frmwork_dev',
+            message: 'Enter the development framework:'
+        }
+    ];
+
+    const answers = await inquirer.prompt(questions);
+    return answers;
+}
+
+async function main() {
+    const answers = await getSchema();
+    console.log('Answers:', answers);
+}
+
+main();
